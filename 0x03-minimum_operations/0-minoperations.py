0168355f629f5f14256ca9120dcfdc8699ff7a81
jfbm74 <jfbm74@gmail.com>
2021-05-13 18:56:55 -0500

    Minimum operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""Minimum operations"""
+"""
+Minimum operations
+"""
 
 
 def minOperations(n):
-    """Minimum operations"""
+    """
+    Minimum operations
+    """
     if type(n) is not int or n <= 1:
         return 0
-    num_h = 1
-    cp = 1
-    op = 1
-    while num_h < n:
-        if n % num_h == 0 and num_h != cp:
-            copy = num_h
-            op += 1
-        num_h = num_h + cp
-        op += 1
-    if num_h == n:
-        return op
-    return 0
+    accu = []
+    div = 2
+    while (n % div) is 0 and (n // div) is not 1:
+        accu.append(div)
+        n = n // div
+    div = 3
+    while n > div:
+        while (n % div) is 0 and (n // div) is not 1:
+            accu.append(div)
+            n = n // div
+        div += 2
+    accu.append(n)
+    return sum(accu)
