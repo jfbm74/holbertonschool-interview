c7af8ca378092339b0979949d4712b1e58c7f0eb
jfbm74 <jfbm74@gmail.com>
2021-09-19 16:50:58 -0500

    array to avl

---
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -0,0 +1,34 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer store
+ * @parent: Point
+ * @left: Pointb end
+ * @right: Pointer  right
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s avl_t;
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *splice_array(int *array, size_t start, size_t end, avl_t *parent);
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif
